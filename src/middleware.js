aa74adbe942192654e29cd681e9d044b1cf28b64
Shreyash Srivastava <shreyashsr2004@gmail.com>
2026-09-08 11:17:30 +0530

    feat: add middleware to redirect legacy vercel domains to primary domain

--- src/middleware.js
+++ src/middleware.js
@@ -11,16 +11,18 @@ export function middleware(request) {
     return NextResponse.redirect(url, 301);
   }
 
+  return NextResponse.next();
+}
 
-  export const config = {
-    matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       */
-      '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
-  };
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
